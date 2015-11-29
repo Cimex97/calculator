1745fbd3443e3e805d015f569759f683393a0784
Stephan <stephan.alsleben@hotmail.de>
2015-11-29 19:27:53 +0100

    code auskommentiert der nicht benötigt wird momentan

--- quelldatein/parsen.cpp
+++ quelldatein/parsen.cpp
@@ -6,12 +6,12 @@ int parsen(std::string rechnung){      //eingabe parsen
 
   struct rechenzeichen zeichen;
   
-  int posplus = rechnung.find(zeichen.plus);                           //Position der Rechenzeichen suchen
+  /*  int posplus = rechnung.find(zeichen.plus);                           //Position der Rechenzeichen suchen
   int posminus = rechnung.find(zeichen.minus);
   int posmal = rechnung.find(zeichen.mal);
   int posgeteielt = rechnung.find(zeichen.geteielt);
   int posquadrat = rechnung.find(zeichen.quadrat);
-  int leange = rechnung.length();
+  int leange = rechnung.length();*/
   
 
   std::cout << std::endl << "Laenge: " << leange << std::endl;
