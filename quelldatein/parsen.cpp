d250fac0d0bdccad955b63a8d06e1885cc3b22d8
Stephan <stephan.alsleben@hotmail.de>
2015-11-29 19:17:13 +0100

    auslesen der eingabe buchstabe für buchstabe

--- quelldatein/parsen.cpp
+++ quelldatein/parsen.cpp
@@ -5,19 +5,27 @@
 int parsen(std::string rechnung){      //eingabe parsen
 
   struct rechenzeichen zeichen;
-  std::cout << rechnung << std::endl;
-
+  
   int posplus = rechnung.find(zeichen.plus);                           //Position der Rechenzeichen suchen
   int posminus = rechnung.find(zeichen.minus);
   int posmal = rechnung.find(zeichen.mal);
   int posgeteielt = rechnung.find(zeichen.geteielt);
   int posquadrat = rechnung.find(zeichen.quadrat);
+  int leange = rechnung.length();
+  
 
-  std::cout << "Position plus: " << posplus << std::endl                //Positionen der Rechenzeichen anzeigen(Funktioniert nur bei einem jeder sorte)
-	    << "Position minus: " << posminus << std::endl
-	    << "Position mal: " << posmal <<std::endl
-	    << "Position geteielt: " << posgeteielt << std::endl
-	    << "Position quadrat: " << posquadrat << std::endl;
+  std::cout << std::endl << "Laenge: " << leange << std::endl;
+  
+  for(int i=0;i<leange;i++){
+    std::cout << "Pointer vom String: " << rechnung.at(i) << std::endl;  //rechnung.at() gibt den string buchstabe fuer buchstabe aus
+    switch(rechnung.at(i))
+      {
+      case '+': std::cout << "Ein Plus wurde gelesen" << std::endl << std::endl;
+	break;
+      case '-': std::cout << "Ein Minus wurde gelesen" << std::endl <<std::endl;
+	break;
+      }
+  }
   
 
   return ERFOLGREICH;
