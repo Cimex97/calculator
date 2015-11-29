2f5b688fa768da32ce6cd01574e96d182638fad1
Stephan <stephan.alsleben@hotmail.de>
2015-11-29 12:57:01 +0100

    im string kann nun nach den Rechenzeichen gesucht werden(nur eins nicht 2 von einer sorte)

--- quelldatein/parsen.cpp
+++ quelldatein/parsen.cpp
@@ -5,9 +5,22 @@
 int parsen(std::string rechnung){      //eingabe parsen
 
   struct rechenzeichen zeichen;
-  
   std::cout << rechnung << std::endl;
-  int laenge = rechnung.size();
-  std::cout << "Laenge der Rechnung " << laenge << std::endl;
+
+  int posplus = rechnung.find(zeichen.plus);                           //Position der Rechenzeichen suchen
+  int posminus = rechnung.find(zeichen.minus);
+  int posmal = rechnung.find(zeichen.mal);
+  int posgeteielt = rechnung.find(zeichen.geteielt);
+  int posquadrat = rechnung.find(zeichen.quadrat);
+
+  std::cout << "Position plus: " << posplus << std::endl                //Positionen der Rechenzeichen anzeigen(Funktioniert nur bei einem jeder sorte)
+	    << "Position minus: " << posminus << std::endl
+	    << "Position mal: " << posmal <<std::endl
+	    << "Position geteielt: " << posgeteielt << std::endl
+	    << "Position quadrat: " << posquadrat << std::endl;
+  
+
   return ERFOLGREICH;
 }
+
+
