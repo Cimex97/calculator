74150b9aadc76f719b08b33f5a1529ddc6ed4f57
Stephan <stephan.alsleben@hotmail.de>
2015-12-02 21:01:15 +0100

    parsen der eingabe realisiert(momentan nur mit plus möglich)

--- quelldatein/parsen.cpp
+++ quelldatein/parsen.cpp
@@ -2,25 +2,40 @@
 #include <string>
 #include "../headerdatein/parsen.h"
 
-int parsen(std::string rechnung){      //eingabe parsen
+#define MAX_SIZE 60
+
+int parsen(std::string rechnung)
+{                                                                         //eingabe parsen
 
   struct rechenzeichen zeichen;
   
   /*  int posplus = rechnung.find(zeichen.plus);                           //Position der Rechenzeichen suchen
-  int posminus = rechnung.find(zeichen.minus);
-  int posmal = rechnung.find(zeichen.mal);
-  int posgeteielt = rechnung.find(zeichen.geteielt);
-  int posquadrat = rechnung.find(zeichen.quadrat);
-  int leange = rechnung.length();*/
+      int posminus = rechnung.find(zeichen.minus);
+      int posmal = rechnung.find(zeichen.mal);
+      int posgeteielt = rechnung.find(zeichen.geteielt);
+      int posquadrat = rechnung.find(zeichen.quadrat);*/
+  int leange = rechnung.length();
+  int start = 0;
+  int ende = 0;
+  int count = 0; 
+  std::string zahlen[MAX_SIZE];
+  std::string mathe[MAX_SIZE];
   
-
   std::cout << std::endl << "Laenge: " << leange << std::endl;
   
   for(int i=0;i<leange;i++){
-    std::cout << "Pointer vom String: " << rechnung.at(i) << std::endl;  //rechnung.at() gibt den string buchstabe fuer buchstabe aus
     switch(rechnung.at(i))
       {
-      case '+': std::cout << "Ein Plus wurde gelesen" << std::endl << std::endl;
+      case '+':
+	ende = i - start;
+	//std::cout << rechnung.substr(start, ende) <<"   start: " << start << "    i: " << i << std::endl ;
+	zahlen[count] = rechnung.substr(start, ende);
+	mathe[count] = rechnung.substr((start+ende),1);
+	start = ++i;
+	std::cout << zahlen[count] << std::endl
+		  << mathe[count] << std::endl;
+	count = ++count;
+	
 	break;
       case '-': std::cout << "Ein Minus wurde gelesen" << std::endl <<std::endl;
 	break;
